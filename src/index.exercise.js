37b8527bcee22baad463444cf3afc955e16419dc
Kim Sheng Yong <kimshengyong@gmail.com>
2021-06-19 13:02:58 +0800

    229: add dialog for login and register

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,6 +1,8 @@
-// 🐨 you'll need to import React and ReactDOM up here
+// 🐨 you'll need to import React and ReactDOM up here\
+import '@reach/dialog/styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Dialog} from '@reach/dialog'
 // 🐨 you'll also need to import the Logo component from './components/logo'
 import {Logo} from './components/logo'
 // 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
@@ -8,16 +10,29 @@ import {Logo} from './components/logo'
 
 // 🐨 use ReactDOM to render the <App /> to the root element
 function App() {
+  const [openModal, setOpenModal] = React.useState('none')
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('Login clicked')}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button onClick={() => alert('Register clicked')}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog aria-label="Login Form" isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>CLose</button>
+        </div>
+        Login
+      </Dialog>
+      <Dialog aria-label="Register Form" isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>CLose</button>
+        </div>
+        Register
+      </Dialog>
     </div>
   )
 }
