7ea04dbafd3eee78e2bed0af44524fa6a84de56f
Kim Sheng Yong <kimshengyong@gmail.com>
2021-06-19 13:29:15 +0800

    230: add form for login and register

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -7,10 +7,40 @@ import {Dialog} from '@reach/dialog'
 import {Logo} from './components/logo'
 // 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
 // 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
-
+function LoginForm({onSubmit, buttonText}) {
+  function handleSubmit(event) {
+    event.preventDefault()
+    const {username, password} = event.target.elements
+    onSubmit({
+      useraname: username.value,
+      password: password.value,
+    })
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" />
+      </div>
+      <div>
+        <button type="submit">{buttonText}</button>
+      </div>
+    </form>
+  )
+}
 // 🐨 use ReactDOM to render the <App /> to the root element
 function App() {
   const [openModal, setOpenModal] = React.useState('none')
+  function login(formData) {
+    console.log(formData)
+  }
+  function register(formData) {
+    console.log(formData)
+  }
   return (
     <div>
       <Logo width="80" height="80" />
@@ -23,15 +53,17 @@ function App() {
       </div>
       <Dialog aria-label="Login Form" isOpen={openModal === 'login'}>
         <div>
-          <button onClick={() => setOpenModal('none')}>CLose</button>
+          <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
         Login
+        <LoginForm onSubmit={login} buttonText="Login" />
       </Dialog>
       <Dialog aria-label="Register Form" isOpen={openModal === 'register'}>
         <div>
-          <button onClick={() => setOpenModal('none')}>CLose</button>
+          <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
         Register
+        <LoginForm onSubmit={register} buttonText="Submit" />
       </Dialog>
     </div>
   )
